84ca29bca6f04aa6f1c378a9de51091a6df82022
NoSoul <danilasoloviev.moscow@yandex.ru>
2023-11-26 23:20:07 +0300

    HW2

--- Soloviev_IDB_22_04/My project/Assets/Scripts/NewBehaviourScript.cs
+++ Soloviev_IDB_22_04/My project/Assets/Scripts/NewBehaviourScript.cs
@@ -5,19 +5,26 @@ using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public Text x;
+    public int c;
     public GameObject Testobj;
     bool a = true;
 
     public void Deactive()
     { 
         if (a == true){
-        Testobj.SetActive(false);
-        a = false;
+            Testobj.SetActive(false);
+            a = false;
         }
         else{
-        Testobj.SetActive(true);
-        a = true;
+            Testobj.SetActive(true);
+            a = true;
     }
     }
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("CEN");
+        c-=1;
+        x.text = c.ToString();
+    }
 }
